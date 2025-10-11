727648b3349b20ea50013875f7b612c27ad18db9
Disruptor07 <2025310619@stu.hit.edu.cn>
2025-10-11 21:11:51 +0800

    使用WSL和VSCode编程

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    ++a;
+    printf("%d",a);
+    printf("你好");
+    return 0;
+}
--- 字符串系统练习（除函数）.c
+++ 字符串系统练习（除函数）.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	char a[]="hello\n";
-	printf("%s",a);
+	char a[]="hello";
+	printf("%s\n",a);
+	printf("%c\n",a[1]);
 	
 	char b[20];
 	fgets(b,sizeof(b),stdin);
